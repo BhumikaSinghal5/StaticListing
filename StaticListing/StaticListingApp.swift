8261270b370cc6a337c219cfe917f04e10717ff4
Bhumika Singhal <bhumika.singhal5@gmail.com>
2025-03-28 17:00:54 +0530

    Initial Commit

---
+++ StaticListing/StaticListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticListingApp.swift
+//  StaticListing
+//
+//  Created by Bhumika Singhal on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct StaticListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StaticListingTests/StaticListingTests.swift
@@ -0,0 +1,16 @@
+//
+//  StaticListingTests.swift
+//  StaticListingTests
+//
+//  Created by Bhumika Singhal on 28/03/25.
+//
+
+import Testing
+
+struct StaticListingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
